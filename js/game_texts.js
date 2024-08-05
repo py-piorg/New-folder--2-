362a0658e9cab5014ef5501949e720e14037d2e6
Kobe <titanic220am@gmail.com>
2024-08-05 08:08:42 -0400

    first

---
+++ js/game_texts.js
@@ -0,0 +1,34 @@
+var mainFontName = { 
+    "en": "Lilita One"
+}
+
+var gameTexts = { 
+    "en": [
+        ["START", 27],
+        ["SELECT\nGAME MODE", 60],
+        ["CLASSIC", 27],
+        ["ACTION", 27],
+        ["PAUSE", 80],
+        ["RESUME", 27],
+        ["RESET", 29],
+        ["SELECTION", 15],
+        ["SELECT\nCOLOR", 26],
+        ["SCORE", 40, 27],
+        ["LEVEL", 27],
+        ["LEVEL\nCOMPLETE", 70],
+        ["NEXT LEVEL", 27],
+        ["END GAME", 27],
+        ["REALLY?", 27],
+        ["YES", 27],
+        ["NO", 27],
+        ["GAME OVER", 60],
+        ["MAIN MENU", 27],
+        ["HELP", 27],
+        ["CLOSE HELP", 27],
+        ["Select a combination of numbers to equal value\ndisplayed in rectangle on the top of the screen.", 25],
+        ["To get bonus points select as much numbers\nas you can, try select numbers of the same\ncolor or clear game area before time runs out.", 25],
+        ["Keep in mind boxes selection order. Selection\ncan't start with a divider or multiplier.", 25],
+        ["The game ends when boxes fill head to the top.", 25],
+        ["MORE GAMES", 27]
+    ]
+}
